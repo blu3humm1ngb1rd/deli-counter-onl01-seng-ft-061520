686e6e29a7aeea628d6a1d6a402061d0a38d387f
BJ Lilley <bethjeanlilley@gmail.com>
2020-06-18 18:53:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(katz_deli)
+  deli_line = []
+  if katz_deli.empty?
+    puts "The line is currently empty."
+else 
+  katz_deli.each.with_index(1) do |name, index|
+    deli_line << "#{index}. #{name}"
+    end
+  puts "The line is currently: #{deli_line.join" "}"
+  end
+  
+end
+
+def take_a_number(katz_deli, new_customer)
+  # 2 arguments
+  #puts to call the person's name and position (starting at 1)
+end
+#def take_a_number(katz_deli, name)
+ # katz_deli << 
+ # if katz_delikatz_deli.each.with_index(1) do |num|
+    #"You are number #{num} in line
+#puts "Welcome, #{name}, you are number #{index} in line."
+
+#end 
